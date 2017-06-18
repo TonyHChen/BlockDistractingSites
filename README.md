549ab8d5a9ec49c84db6635258f385e14dce7630
Tony Chen <chen.tony.h@gmail.com>
2017-06-18 10:19:39 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Block Distracting Sites Chrome Extension
+
+v0.1.0 (6/18/17) Put an overlay in front of distracting sites to prevent usage.
---
+++ content.js
@@ -0,0 +1,5 @@
+var div = document.createElement("div");
+document.body.appendChild(div);
+div.id = "overlay"
+div.innerText = "Get Back to Work!";
+
